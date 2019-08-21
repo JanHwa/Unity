060b1f64827e791f5516d6a1395ae32245c4cdff
Ethan <jianhuatung@gmail.com>
2019-08-21 17:11:30 +0800

    添加Delegate&event的Demo

---
+++ DELEGATE EVENT/Assets/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player.onEnemyHit += Damage;
+    }
+
+    void Damage(Color color)
+    {
+        transform.GetComponent<Renderer>().material.color = color;
+
+    }
+
+    private void OnDisable()
+    {
+        Player.onEnemyHit -= Damage;
+    }
+}
---
+++ DELEGATE EVENT/Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region Singleton
+    private static Player _instance;
+    public static Player Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Player();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    #endregion
+
+    public delegate void ChangeEnemyColor(Color color);
+    public static event ChangeEnemyColor onEnemyHit;
+
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(onEnemyHit != null)
+            {
+
+                onEnemyHit(Color.red);
+            }
+        }
+        
+    }
+}
