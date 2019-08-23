12965039f7f51660746c8eceb8556ee1a28637a2
Ethan <jianhuatung@gmail.com>
2019-08-23 19:07:44 +0800

    添加Admob的sdk

---
+++ AdmobDemo/Assets/GoogleAd.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAd : MonoBehaviour
+{
+
+    public string adUnitId = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        AdRequest request = new AdRequest.Builder().Build();
+        bannerView.LoadAd(request);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnButtonClick()
+    {
+
+    }
+}
